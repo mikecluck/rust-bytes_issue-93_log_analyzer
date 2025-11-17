ef0bc6694e27624428956171d07bac7af312f219
Mike Cluck <MCluck90@gmail.com>
2025-11-17 11:24:52 -0500

    Add logic for analyzing logs

---
+++ src/logs.rs
@@ -0,0 +1,115 @@
+use std::collections::HashMap;
+
+use serde::Serialize;
+use stopwords::Stopwords;
+
+pub(crate) struct LogEntry {
+    pub timestamp: String,
+    pub hostname: String,
+    pub process_name: String,
+    pub pid: u32,
+    pub message: String,
+}
+
+pub(crate) struct LogStatsBuilder {
+    total_entries: usize,
+    by_process: HashMap<String, u32>,
+    by_hostname: HashMap<String, u32>,
+    most_frequent_process: String,
+    most_frequent_hostname: String,
+    top_keywords: Vec<String>,
+    keyword_count: HashMap<String, u32>,
+}
+
+#[derive(Serialize)]
+pub(crate) struct LogStats {
+    pub total_entries: usize,
+    pub by_process: HashMap<String, u32>,
+    pub by_hostname: HashMap<String, u32>,
+    pub most_frequent_process: String,
+    pub most_frequent_hostname: String,
+    pub top_keywords: Vec<String>,
+}
+
+impl LogStatsBuilder {
+    pub fn new() -> Self {
+        Self {
+            total_entries: 0,
+            by_process: HashMap::new(),
+            by_hostname: HashMap::new(),
+            most_frequent_process: String::new(),
+            most_frequent_hostname: String::new(),
+            top_keywords: Vec::new(),
+            keyword_count: HashMap::new(),
+        }
+    }
+
+    pub fn add_log_entry(&mut self, log_entry: LogEntry) {
+        self.total_entries += 1;
+
+        let by_process_count = self.by_process.get(&log_entry.process_name).unwrap_or(&0);
+        self.by_process
+            .insert(log_entry.process_name, *by_process_count + 1);
+
+        let by_hostname_count = self.by_hostname.get(&log_entry.hostname).unwrap_or(&0);
+        self.by_hostname
+            .insert(log_entry.hostname, *by_hostname_count + 1);
+
+        // Keep track of how many times each keyword appears
+        let stopwords = stopwords::NLTK::stopwords(stopwords::Language::English).unwrap();
+        let keywords = log_entry
+            .message
+            .split(|c: char| !c.is_alphabetic())
+            .map(|s| s.to_lowercase())
+            .filter(|s| !stopwords.contains(&s.as_str()));
+
+        for keyword in keywords {
+            let count = self.keyword_count.get(&keyword).unwrap_or(&0);
+            self.keyword_count.insert(keyword, count + 1);
+        }
+    }
+
+    /// Calculate stats about the whole collection
+    pub fn build(mut self) -> LogStats {
+        let mut most_frequent_process_name = "";
+        let mut most_frequent_process_count: u32 = 0;
+        for (process, count) in &self.by_process {
+            if count > &most_frequent_process_count {
+                most_frequent_process_count = *count;
+                most_frequent_process_name = process;
+            }
+        }
+
+        self.most_frequent_process = most_frequent_process_name.to_string();
+
+        let mut most_frequent_hostname_name = "";
+        let mut most_frequent_hostname_count: u32 = 0;
+        for (hostname, count) in &self.by_hostname {
+            if count > &most_frequent_hostname_count {
+                most_frequent_hostname_count = *count;
+                most_frequent_hostname_name = hostname;
+            }
+        }
+
+        self.most_frequent_hostname = most_frequent_hostname_name.to_string();
+
+        let max_count = self.keyword_count.values().max().unwrap();
+        let mut sorted_keywords = self
+            .keyword_count
+            .iter()
+            .filter(|(_, count)| count == &max_count)
+            .map(|(name, _)| name.to_string())
+            .collect::<Vec<_>>();
+        sorted_keywords.sort();
+        self.top_keywords = sorted_keywords;
+
+        LogStats {
+            total_entries: self.total_entries,
+            by_process: self.by_process,
+            by_hostname: self.by_hostname,
+            most_frequent_process: self.most_frequent_process,
+            most_frequent_hostname: self.most_frequent_hostname,
+            top_keywords: self.top_keywords,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,106 @@
 // Rust Bytes Challenge Issue #93 Log Analyzer
+mod logs;
 
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::logs::{LogEntry, LogStatsBuilder};
+
+    #[test]
+    fn handle_one_log_entry() {
+        let mut stats = LogStatsBuilder::new();
+        stats.add_log_entry(LogEntry {
+            timestamp: "Jul 1 09:00:55".into(),
+            hostname: "calvisitor-10-105-160-95".into(),
+            process_name: "kernel".into(),
+            pid: 43,
+            message: "Alfa Bravo Charlie Delta Echo".into(),
+        });
+        let stats = stats.build();
+
+        assert_eq!(stats.total_entries, 1);
+        assert_eq!(stats.by_process["kernel"], 1);
+        assert_eq!(stats.by_hostname["calvisitor-10-105-160-95"], 1);
+        assert_eq!(stats.most_frequent_process, "kernel");
+        assert_eq!(stats.most_frequent_hostname, "calvisitor-10-105-160-95");
+        assert_eq!(
+            stats.top_keywords,
+            vec!["alfa", "bravo", "charlie", "delta", "echo"]
+        );
+
+        let mut stats = LogStatsBuilder::new();
+        stats.add_log_entry(LogEntry {
+            timestamp: "Jul 1 09:00:55".into(),
+            hostname: "home".into(),
+            process_name: "netbiosd".into(),
+            pid: 43,
+            message: "Alfa Bravo Charlie Alfa Bravo".into(),
+        });
+        let stats = stats.build();
+
+        assert_eq!(stats.total_entries, 1);
+        assert_eq!(stats.by_process["netbiosd"], 1);
+        assert_eq!(stats.by_hostname["home"], 1);
+        assert_eq!(stats.most_frequent_process, "netbiosd");
+        assert_eq!(stats.most_frequent_hostname, "home");
+        assert_eq!(stats.top_keywords, vec!["alfa", "bravo"]);
+    }
+
+    #[test]
+    fn handle_two_entries() {
+        let mut stats = LogStatsBuilder::new();
+        stats.add_log_entry(LogEntry {
+            timestamp: "Jul 1 09:00:55".into(),
+            hostname: "hostname_1".into(),
+            process_name: "process_1".into(),
+            pid: 1,
+            message: "Alfa Charlie".into(),
+        });
+        stats.add_log_entry(LogEntry {
+            timestamp: "Jul 1 09:00:55".into(),
+            hostname: "hostname_1".into(),
+            process_name: "process_1".into(),
+            pid: 1,
+            message: "Alfa Bravo".into(),
+        });
+        let stats = stats.build();
+
+        assert_eq!(stats.total_entries, 2);
+        assert_eq!(stats.by_process["process_1"], 2);
+        assert_eq!(stats.by_hostname["hostname_1"], 2);
+        assert_eq!(stats.most_frequent_process, "process_1");
+        assert_eq!(stats.most_frequent_hostname, "hostname_1");
+        assert_eq!(stats.top_keywords, vec!["alfa"]);
+
+        let mut stats = LogStatsBuilder::new();
+        stats.add_log_entry(LogEntry {
+            timestamp: "Jul 1 09:00:55".into(),
+            hostname: "hostname_1".into(),
+            process_name: "process_1".into(),
+            pid: 1,
+            message: "Alfa Charlie".into(),
+        });
+        stats.add_log_entry(LogEntry {
+            timestamp: "Jul 1 09:00:55".into(),
+            hostname: "hostname_1".into(),
+            process_name: "process_2".into(),
+            pid: 1,
+            message: "Bravo Delta".into(),
+        });
+        let stats = stats.build();
+
+        assert_eq!(stats.total_entries, 2);
+        assert_eq!(stats.by_process["process_1"], 1);
+        assert_eq!(stats.by_process["process_2"], 1);
+        assert_eq!(stats.by_hostname["hostname_1"], 2);
+        assert_eq!(stats.most_frequent_process, "process_1");
+        assert_eq!(stats.most_frequent_hostname, "hostname_1");
+        assert_eq!(
+            stats.top_keywords,
+            vec!["alfa", "bravo", "charlie", "delta"]
+        );
+    }
 }
